2051e1617db1e9c92d5d00f0875a80287c285998
Armando Martinez <alienangy2004@gmail.com>
2026-09-25 11:52:28 -0600

    progress ESM 2026 Migrate to ES modules and update route handing

--- server/app.js
+++ server/app.js
@@ -11,11 +11,15 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 // Se importan las rutas de la aplicacion
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+// Recreación de __dirname para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Crea la aplicacion de express
-var app = express();
+const app = express();
 
 // Configura el motor de vistas
 app.set('views', path.join(__dirname, 'views'));
@@ -35,16 +39,16 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+export default app;
